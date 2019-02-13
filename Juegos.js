f005a210bb6bc09854df049a7c986be8bfc31898
jcarrascalnegrette <36543029+jcarrascalnegrette@users.noreply.github.com>
2019-02-13 08:41:36 -0500

    Add files via upload

---
+++ Juegos.js
@@ -0,0 +1,52 @@
+//importamos
+const express = require("express");
+const mongoose = require("mongoose");
+//instanciamos
+const app = express();
+const bodyParser = require("body-parser");
+//midleware
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+//conectar a la base de datos
+mongoose
+  .connect(
+    // "mongodb://localhost:27017/gouniversity",
+    "mongodb+srv://juego:juego01@cluster0-rdniu.mongodb.net/test?retryWrites=true",
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("connected");
+  })
+  .catch(err => {
+    console.log("err", err);
+  });
+
+//modulos externos
+var Juegos = require("./lib01/Juego");
+
+app.get("/Juegos", (req, res) => {
+  Juegos.getJuego(req, res);
+});
+
+app.get("/Juegos/:id", (req, res) => {
+  Juegos.getJuego(req, res);
+});
+
+app.post("/Juegos", (req, res) => {
+  Juegos.newJuego(req, res);
+});
+app.put("/agendas/:id", (req, res) => {
+  Juegos.updateJuego(req, res);
+});
+
+app.delete("/Juegos/:id", (req, res) => {
+  Juegos.deleteJuego(req, res);
+});
+
+// escuchamos
+app.listen(9000);
+console.log(`Server on %s ${app.settings.env}`);
